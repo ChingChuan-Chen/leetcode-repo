80f68310da5d0edfeaacb2d20e28d19ce03200b3
Ching-Chuan Chen <zw12356@gmail.com>
2020-08-17 01:49:49 +0800

    add 15, 167

--- Python/S0015-3sum.py
+++ Python/S0015-3sum.py
@@ -1,5 +1,6 @@
 """
-Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0?
+Find all unique triplets in the array which gives the sum of zero.
 
 Note:
 The solution set must not contain duplicate triplets.
@@ -13,16 +14,34 @@ A solution set is:
   [-1, 0, 1],
   [-1, -1, 2]
 ]
-
-
 """
 from typing import List
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
-        pass
+        res = []
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+            if i == 0 or nums[i] != nums[i-1]:
+                self.twoSumII(nums, i, res)
+        return res
 
+    def twoSumII(self, nums: List[int], i: int, res: List[List[int]]) -> None:
+        low = i + 1
+        high = len(nums) - 1
+        while low < high:
+            sum = nums[i] + nums[low] + nums[high]
+            if sum < 0:
+                low += 1
+            elif sum > 0:
+                high -= 1
+            else:
+                res.append([nums[i], nums[low], nums[high]])
+                low += 1
+                high -= 1
+                while low < high and nums[low] == nums[low-1]:
+                    low += 1
 
 if __name__ == '__main__':
-    assert Solution().threeSum(0) == 0
-
+    assert Solution().threeSum([-1, 0, 1, 2, -1, -4]) == [[-1, 0, 1], [-1, -1, 2]]
--- src-all/Python/S0167-two-sum-ii-input-array-is-sorted.py
+++ src-all/Python/S0167-two-sum-ii-input-array-is-sorted.py
@@ -1,26 +1,37 @@
 """
-Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+Given an array of integers that is already sorted in ascending order,
+find two numbers such that they add up to a specific target number.
 
-The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+The function twoSum should return indices of the two numbers such that they add up to the target,
+where index1 must be less than index2.
 
 Note:
 	Your returned answers (both index1 and index2) are not zero-based.
-	You may assume that each input would have exactly one solution and you may not use the same element twice.
+	You may assume that each input would have exactly one solution
+	and you may not use the same element twice.
 
 Example:
-
 Input: numbers = [2,7,11,15], target = 9
 Output: [1,2]
 Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
-
 """
 from typing import List
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        
-        pass
+        low = 0
+        high = len(numbers) - 1
+        while low < high:
+            sum = numbers[low] + numbers[high]
+            if sum == target:
+                return [low+1, high+1]
+            elif sum < target:
+                low += 1
+            else:
+                high -= 1
+        return [-1, -1]
 
 
 if __name__ == '__main__':
-    assert Solution().twoSum(0) == 0
-
+    assert Solution().twoSum([2, 7, 11, 15], 9) == [1, 2]
+    assert Solution().twoSum([2, 7, 11, 15], 18) == [2, 3]
+    assert Solution().twoSum([2, 7, 11, 15], 12) == [-1, -1]
